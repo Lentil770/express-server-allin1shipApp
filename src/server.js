fdfbaf5948a8ac7a26099c574a33f41801a55562
Lentil770 <you@example.com>
2021-02-04 15:49:51 -0500

    attempt to fix fetching wrong stops to app

--- src/server.js
+++ src/server.js
@@ -99,7 +99,7 @@ app.get('/getDailySchedule/:user', (req, res) => {
     customers.customer_name, customers.address, customers.location, customers.contact_name, customers.contact_number, customers.comments
     FROM schedules 
     JOIN schedule_stop_table ON (schedule_stop_table.schedule_id) = (schedules.id)
-    JOIN customers on stops.customer_id = customers.customer_id
+    JOIN customers schedule_stop_table.customer_id = customers.customer_id
     WHERE schedule_date >= '${todaysDate} 08:00:00' AND schedule_date < '${tomorrowsDate} 08:00:00' AND driver = '${driver}'
     ORDER BY stop_number;`;
 
