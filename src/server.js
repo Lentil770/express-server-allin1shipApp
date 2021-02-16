a5ea09dbdd867e0a96c004feb973841480c9dbb0
Lentil770 <you@example.com>
2021-02-16 13:42:36 -0500

    fetch completion status

--- src/server.js
+++ src/server.js
@@ -123,7 +123,8 @@ app.get('/getDailySchedule/:user', (req, res) => {
   const tomorrowsDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate()+1);
   const driver = req.params.user;
 
-  let sql = `SELECT DISTINCT schedules.vehicle, schedules.driver, schedules.dropoff_info, schedule_stop_table.schedule_stop_id, schedule_stop_table.stop_number,
+  let sql = `SELECT DISTINCT schedules.vehicle, schedules.driver, schedules.dropoff_info, 
+    schedule_stop_table.schedule_stop_id, schedule_stop_table.stop_number, schedule_stop_table.completion_status,
     customers.customer_name, customers.address, customers.location, customers.contact_name, customers.contact_number, customers.comments
     FROM schedules 
     JOIN schedule_stop_table ON schedule_stop_table.schedule_id = schedules.id
